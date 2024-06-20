dd7516c13aee85a16220c2dd29022e7069dd72ca
Boyan Dihonov <boyandihonov@abv.bg>
2024-06-20 17:52:57 +0300

    up

--- FriendlyWorld/src/controllers/catalog.js
+++ FriendlyWorld/src/controllers/catalog.js
@@ -1,11 +1,24 @@
 const { Router } = require('express')
-const { getAll } = require('../services/animal')
+const { getAll, getById } = require('../services/animal')
 
 const catalogRouter = Router()
 
 catalogRouter.get('/catalog', async (req, res) => {
     const animals = await getAll()
     res.render('catalog', { animals })
+})
+
+catalogRouter.get('/catalog/:id', async (req, res) => {
+    const id = req.params.id
+
+    const animal = await getById(id)
+
+    if (!animal) {
+        res.status(404).render('404')
+        return
+    }
+
+    res.render('details', { animal })
 })
 
 module.exports = { catalogRouter }
--- FriendlyWorld/src/index.js
+++ FriendlyWorld/src/index.js
@@ -24,16 +24,63 @@ async function start() {
     });
 }
 
-
+// async function test() {
+//     try {
+//         const result = await register('peter@abv.bg', 'Peter', '123');
+//         const token = createToken(result)
+//         ole.log(token)
+//     } catch (err) {
+//         console.log('Caught error')
+//         console.error(err.message)
+//     }
+// }
 
 // async function test(){
 //     try{
 //        const result = await create('boqn@abv.bg', '123')
 //        const token = createToken(result)
-//        console.log(token)
-//     }catch (err){
+// //        console.log(token)
+// //     }catch (err){
 //         console.log ('Caught error')
 //         console.error(err.message)
 //     }
 
+//  Read Record
+// const result = await api.getById(dataId);
+// console.log(result)
+
 // }
+
+// // Create Record
+
+//     const result = await api.getById(dataId);
+//     console.log(result)
+// // Edit Record
+
+// // const dataId = '6671b1bb0d24bec13e99a69e';
+// const formData ={
+//     name:'Blue',
+//     years:24,
+//     kind:'forest',
+//     image: '/static/images/animal4.jpg',
+//     need:'Medicine',
+//     location:'Ruse',
+//     description:'something',
+//     donations: 'something else'
+
+// }
+
+// Delete Record
+
+// // const dataId = '6671b1bb0d24bec13e99a69e';
+// // const userId = '6671abebe30e1a1c81e127d8';
+
+// // await application.deleteById(dataId,userId);
+
+
+// //Add Vote
+
+// // const userId = '6671abebe30e1a1c81e127d8';
+
+// const result = await api.addVote(dataId, userId)
+
